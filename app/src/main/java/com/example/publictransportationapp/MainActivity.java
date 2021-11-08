711e5805ef28e60b6ec15975e5305e56cd9d213a
FabianTalos <fabiantalos@yahoo.com>
2021-11-08 21:57:44 +0200

    Added Navigation Menu

--- app/src/main/java/com/example/publictransportationapp/MainActivity.java
+++ app/src/main/java/com/example/publictransportationapp/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.publictransportationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -21,13 +25,48 @@ public class MainActivity extends AppCompatActivity {
     Button buttonAddTransport, buttonChange;
     DatabaseReference fbdb;
 
-    float x1, x2, y1, y2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new VehiclesFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_search:
+                    selectedFragment = new SearchFragment();
+                    break;
+                case R.id.nav_vehicles:
+                    selectedFragment = new VehiclesFragment();
+                    break;
+                case R.id.nav_favorites:
+                    selectedFragment = new FavoritesFragment();
+                    break;
+                case R.id.nav_info:
+                    selectedFragment = new InfoFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+
+            return true;
+        }
+    };
+}
+        /*
         vehicleType = findViewById(R.id.vehicleType);
         vehicleNumber = findViewById(R.id.vehicleNumber);
         vehicleStart = findViewById(R.id.stationStart);
@@ -61,5 +100,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Data added to database!", Toast.LENGTH_LONG).show();
             }
         });
-    }
-}
+
+         */
